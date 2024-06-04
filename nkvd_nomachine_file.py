f1054c5857e4c4172884a1c66fe3725eb9fe4aa0
noook <noook@yandex.ru>
2024-06-04 13:10:26 +0700

    #4

--- nkvd_nomachine_file.py
+++ nkvd_nomachine_file.py
@@ -4,39 +4,36 @@
 import xml.etree.ElementTree as ET
 import openpyxl
 
+def get_ip_from_nxs(file_nxs):
+    tree = ET.parse(file_nxs)
+    root = tree.getroot()
+
+    # чтение всех атрибутов в дереве
+    for branch in root:
+        rez = None
+        if branch.attrib['name'] == 'General':
+            # print(f'{branch.tag = } ... {branch.attrib = }')
+
+            for sub_branch in branch:
+                if sub_branch.attrib['key'] == 'Server host' and sub_branch.attrib.get('value'):
+                    # print(f'... {sub_branch.tag = } ... {sub_branch.attrib = }')
+                    # print(sub_branch.attrib)
+                    # if sub_branch.attrib.get('value'):
+                    #     print('!!!!!!!!', sub_branch.attrib.get('Server host'))
+                    rez = (file_nxs, sub_branch.attrib.get('value'))
+                else:
+                    rez =  None
+    return rez
+
 file_nxs = 'res/test1.nxs'
 file_xlsx = 'test1.xlsx'
+
+print(get_ip_from_nxs(file_nxs))
+print(type(get_ip_from_nxs(file_nxs)))
 
 # wb = openpyxl.Workbook()
 # wb_s = wb.active
 # wb_s.append(["IP", "Name"])
-
-tree = ET.parse(file_nxs)
-root = tree.getroot()
-
-# чтение конкретного атрибута
-# print(root[0][1].attrib)
-print()
-
-# # чтение всех атрибутов в дереве
-# for branch in root:
-#     print(f'{branch.tag = } ... {branch.attrib = }')
-#     for sub_branch in branch:
-#         print(f'... {sub_branch.tag = } ... {sub_branch.attrib = }')
-#         # if sub_branch.attrib.get['Server host']:
-#         #     print('!!!!!!!!')
-# print()
-
-# чтение всех атрибутов в дереве
-for branch in root:
-    if branch.attrib['name'] == 'General':
-        for sub_branch in branch:
-            if sub_branch.attrib['key'] == 'Server host':
-                print(f'{branch.tag = } ... {branch.attrib = }')
-                print(f'... {sub_branch.tag = } ... {sub_branch.attrib = }')
-                # if sub_branch.attrib.get['Server host']:
-                #     print('!!!!!!!!')
-print()
 
 
 # for branch in root:
