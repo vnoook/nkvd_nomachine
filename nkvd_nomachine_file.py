c2cfa9bd6410f6e0572aa0805f88141862c587c2
noook <noook@yandex.ru>
2024-06-05 12:33:06 +0700

    #9

--- nkvd_nomachine_file.py
+++ nkvd_nomachine_file.py
@@ -6,7 +6,14 @@ import xml.etree.ElementTree as ET
 import openpyxl
 
 
-def get_ip_from_nxs(file):
+# функция чтения файла nxs, формат xml
+def get_ip_from_nxs(file: str) -> tuple:
+    """
+    функция чтения файла nxs (формат xml)
+    берёт из файла поле "Server host"
+    выдаёт кортеж - файл в котором ищется и ИП-адрес подключения
+    """
+
     tree = ET.parse(file)
     root = tree.getroot()
     rez = None
@@ -18,9 +25,7 @@ def get_ip_from_nxs(file):
                 key = sub_branch.attrib.get('key')
                 val = sub_branch.attrib.get('value')
                 if (key == 'Server host') and val:
-                    # print(os.path.split(file)[1])
                     rez = (file, sub_branch.attrib.get('value'))
-    # print(rez)
     return rez
 
 
@@ -29,8 +34,6 @@ dir_nxs = r'res/'
 file_xlsx = 'test1.xlsx'
 get_ip_from_nxs('res/test1.nxs')
 etx_nxs = '.nxs'
-# print(get_ip_from_nxs('res/test1.nxs'))
-# print(type(get_ip_from_nxs(file_nxs)))
 
 wb = openpyxl.Workbook()
 wb_s = wb.active
@@ -39,13 +42,7 @@ wb_s.append(['IP', 'NAME'])
 os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_nxs))
 for data_of_scan in os.scandir():
     if data_of_scan.is_file() and os.path.splitext(os.path.split(data_of_scan)[1])[1] == etx_nxs:
-        # print(data_of_scan.name)
-        # print(data_of_scan.path)
-        # print(data_of_scan.__class__)
         wb_s.append([get_ip_from_nxs(data_of_scan.name)[1], get_ip_from_nxs(data_of_scan.name)[0]])
-
-        # print(data_of_scan)
-        # os.remove(data_of_scan)
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 wb.save(file_xlsx)
