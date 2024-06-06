5de8c59752d099064f9a9a2b2f4e4d4c2d4f8fdc
noook <noook@yandex.ru>
2024-06-06 16:13:30 +0700

    #14

--- nkvd_nomachine_file.py
+++ nkvd_nomachine_file.py
@@ -4,6 +4,7 @@
 import os
 import xml.etree.ElementTree as ET
 import openpyxl
+import difflib
 
 dir_nxs = r'res/'
 ext_nxs = '.nxs'
@@ -34,6 +35,10 @@ def get_ip_from_nxs(file: str) -> tuple:
     return rez
 
 
+def spliter_name(string_name: str) -> str:
+    return string_name.split('(')[0]
+
+
 # создаётся эксель
 wb = openpyxl.Workbook()
 wb_s = wb.active
@@ -44,16 +49,17 @@ os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_nxs))
 for data_of_scan in os.scandir():
     # если это файл и расширение, то из этого файла берутся данные
     if data_of_scan.is_file() and os.path.splitext(os.path.split(data_of_scan)[1])[1] == ext_nxs:
-        ip_addr = get_ip_from_nxs(data_of_scan.name)[1]
-        name_file = get_ip_from_nxs(data_of_scan.name)[0]
+        ip_addr = get_ip_from_nxs(data_of_scan.name)[1].strip()
+        name_file = spliter_name(get_ip_from_nxs(data_of_scan.name)[0].strip())
 
-        print()
-        print(ip_addr,' --- ', name_file)
+        # print()
+        # print(ip_addr,' --- ', name_file)
         if dict_data_nxs_files.get(ip_addr) is None:
-            print('такого ключа нет ---', dict_data_nxs_files.get(ip_addr))
-            dict_data_nxs_files[ip_addr] = []
+            # print('такого ключа нет ---', dict_data_nxs_files.get(ip_addr))
+            dict_data_nxs_files[ip_addr] = [os.path.splitext(os.path.split(name_file)[1])[0]]
         else:
-            print('такой есть ---', dict_data_nxs_files.get(ip_addr))
+            # print('такой есть ---', dict_data_nxs_files.get(ip_addr))
+            # print(ip_addr, os.path.splitext(os.path.split(name_file)[1])[0])
             dict_data_nxs_files[ip_addr].append(os.path.splitext(os.path.split(name_file)[1])[0])
 
         wb_s.append([ip_addr, name_file])
@@ -62,9 +68,23 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 wb.save(file_xlsx)
 wb.close()
 
+# print()
+# print(dict_data_nxs_files)
+
 print()
-print()
-print(dict_data_nxs_files, sep='\n')
+for key_ip, val_names in dict_data_nxs_files.items():
+    print(key_ip, val_names)
+    for name in val_names:
+        print('.......', name, '.......', spliter_name(name))
+
+
+# # сравнение имён файлов между именем архива и именем csv файла, они должны почти совпадать
+# diff_ratio_file_names = difflib.SequenceMatcher(
+#     None,
+#     take_file[2],
+#     self.parse_file_parts(file_in_zf.filename)[2]).ratio()
+# if diff_ratio_file_names < 0.9:
+#     pass
 
 # print(name_file)  # file path
 # print(os.path.splitext(os.path.split(data_of_scan)[1]))  # file,ext
