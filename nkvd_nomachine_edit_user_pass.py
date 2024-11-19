dffbc954ff8df485a0deb9ac386719cba40f439f
noook <noook@yandex.ru>
2024-11-19 15:41:03 +0700

    #43 add file for edit nomachine`s files

--- nkvd_nomachine_edit_user_pass.py
+++ nkvd_nomachine_edit_user_pass.py
@@ -1,24 +1,23 @@
-# в файле nxs нужно найти строку "Server host" в группе "General"
-# например, группа <group name="General" > и строка "<option key="Server host" value="172.16.40.30" />"
-#                  <group name = "Login" >           <option key="Auth"  value="++++++++++++" />
-#                  <group name = "Login" >           <option key ="User" value = "user" / >
+# <option key="Auth" value="OSn+Fy.Ml#?]s1Of$BPr4D_w@Si&amp;K]t4VQ" />
+# <option key="User" value="user" />
+# PASS = '  <option key="Auth" value="OSn+Fy.Ml#?]s1Of$BPr4D_w@Si&amp;K]t4VQ" />\n'
+# KEY1 = '  <option key="User" value="a_oividutov" />\n'
+# KEY2 = '  <option key="User" value="master" />\n'
+# RES = '  <option key="User" value="user" />\n'
 
 import os
-import xml.etree.ElementTree as ET
-import openpyxl
+import chardet
 
 dir_nxs = r'res/'
 ext_nxs = '.nxs'
-file_xlsx = 'test1.xlsx'
-dict_data_nxs_files = {}
-dict_data_nxs_files_good_names = {}
+flag_edit_file = False
 
 
 # получение файлов с расширением из папки
 def get_files_nxs() -> list:
     list_of_files = None
     for data_of_scan in os.scandir():
-        # если это файл и расширение, то из этого файла берутся данные
+        # если это файл и расширение nxs, то этот файл добавляется в список
         if data_of_scan.is_file() and os.path.splitext(os.path.split(data_of_scan)[1])[1] == ext_nxs:
             full_name = data_of_scan.name
             if list_of_files is None:
@@ -28,108 +27,34 @@ def get_files_nxs() -> list:
     return list_of_files
 
 
-# функция чтения файла nxs, формат xml
-def get_ip_from_nxs(file: str) -> list:
-    """
-    Функция чтения файла nxs (формат xml)
-    берёт из файла поле "Server host"
-    выдаёт кортеж - файл в котором ищется и ИП-адрес подключения
-    """
-
-    tree = ET.parse(file)
-    root = tree.getroot()
-    rez = None
-
-    # чтение всех атрибутов в дереве по-очереди
-    # поиск значений адреса "General-Server host" первым
-    for branch in root:
-        if branch.attrib['name'] == 'General':
-            for sub_branch in branch:
-                key = sub_branch.attrib.get('key')
-                val = sub_branch.attrib.get('value')
-                if key == 'Server host':
-                    rez = [file, val]
-
-    # поиск значений адреса "Login-User" вторым
-    for branch in root:
-        if branch.attrib['name'] == 'Login':
-            for sub_branch in branch:
-                key = sub_branch.attrib.get('key')
-                val = sub_branch.attrib.get('value')
-                if key == 'User':
-                    rez.append(val)
-
-    # поиск значений адреса "Login-Auth" третьим
-    for branch in root:
-        if branch.attrib['name'] == 'Login':
-            for sub_branch in branch:
-                key = sub_branch.attrib.get('key')
-                val = sub_branch.attrib.get('value')
-                if key == 'Auth':
-                    rez.append(val)
-
-    return rez
+# получение кодировки файла
+def get_codepage(one_file):
+    detector = chardet.universaldetector.UniversalDetector()
+    with open(one_file, 'rb') as fh:
+        for line in fh:
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+    return detector.result['encoding']
 
 
-# функция извлечения из имени файла подстроки до символа кавычек
-def spliter_name(string_name: str) -> str:
-    return string_name.rsplit('(', 1)[0]
-
-
-# функция поиска вхождения строки в список строк имён
-def contain_name(string_name: str, set_names: set) -> bool:
-    answer = False
-    for name in set_names:
-        # if (string_name in name) and len(string_name) < len(name):
-        if string_name in name:
-            answer = True
-    return answer
-
-
-# создаётся эксель
-wb = openpyxl.Workbook()
-wb_s = wb.active
-wb_s.append(['IP', 'NAME'])
-
-# переход в папку для файлов и поиск в ней файлов
-# создание двух словарей - реальные файлы и файлы с нужными названиями для слияний
+# переход в папку для файлов
 os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_nxs))
-for full_name_file in get_files_nxs():
-    name_file = os.path.splitext(os.path.split(full_name_file)[1])[0]
-    good_name_file = spliter_name(name_file)
-    ip_addr = get_ip_from_nxs(full_name_file)[1].strip()
+# и поиск в ней файлов
+for full_name_nxs_file in get_files_nxs():
+    print(full_name_nxs_file, ',', get_codepage(full_name_nxs_file))
+    flag_edit_file = False
 
-    # создание словаря с реальными файлами
-    if dict_data_nxs_files.get(ip_addr) is None:
-        dict_data_nxs_files[ip_addr] = [full_name_file]
-    else:
-        dict_data_nxs_files[ip_addr].append(full_name_file)
 
-    # создание и добавление в словарь списка уже подготовленных коротких имён
-    if dict_data_nxs_files_good_names.get(ip_addr) is None:
-        dict_data_nxs_files_good_names[ip_addr] = {good_name_file}
-    else:
-        if 'Подключение' not in good_name_file:
-            if not contain_name(good_name_file, dict_data_nxs_files_good_names[ip_addr]):
-                dict_data_nxs_files_good_names[ip_addr].add(good_name_file)
+# # чтение построчно файла
+# with open(full_name_nxs_file, encoding='cp1251', newline='') as nxs_file:
+#     row_csv_content = csv.reader(csvfile, delimiter=';')
 
-    wb_s.append([ip_addr, good_name_file])
+# with open(filename, 'r') as file_html:
+#     all_strings_file = file_html.read()
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-wb.save(file_xlsx)
-wb.close()
+# with open(filename.text(), 'r') as file_html:
+#     list_each_string_of_file = file_html.read().splitlines()
 
-print()
-os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_nxs))
-for key_ip, val_names in dict_data_nxs_files.items():
-    new_name = ' '.join(dict_data_nxs_files_good_names[key_ip]).strip()+'_.nxs'
-    print(val_names)
-    if len(val_names) > 1:
-        for real_file in val_names:
-            if val_names.index(real_file) == 0:
-                print('переименовываю "'+real_file+'" в "'+new_name+'"')
-                os.rename(real_file, new_name)
-            else:
-                print('удаляю', real_file)
-                os.remove(real_file)
-        print()
+# end
