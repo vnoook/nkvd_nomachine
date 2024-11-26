388eb5c1a91ea32d7260c8ac67ae4af9999e2898
noook <noook@yandex.ru>
2024-11-26 15:49:19 +0700

    #60

---
+++ nkvd_nomachine_duplicate_ip.py
@@ -0,0 +1,80 @@
+# в файле nxs нужно найти строку "Server host" в группе "General"
+# например, группа <group name="General" > и строка "<option key="Server host" value="172.16.40.30" />"
+
+import os
+import xml.etree.ElementTree as ET
+
+dir_nxs = r'res/'
+ext_nxs = '.nxs'
+list_of_nxs_files = []
+
+
+# получение файлов с расширением из текущей папки
+def get_files_nxs() -> list:
+    list_of_files = None
+    for data_of_scan in os.scandir():
+        # если это файл и расширение, то из этого файла берутся данные
+        if data_of_scan.is_file() and os.path.splitext(os.path.split(data_of_scan)[1])[1] == ext_nxs:
+            full_name = data_of_scan.name
+            if list_of_files is None:
+                list_of_files = []
+            else:
+                list_of_files.append(full_name)
+    return list_of_files
+
+
+# функция чтения файла nxs, формат xml
+def get_ip_from_nxs(file: str) -> list:
+    """
+    Функция чтения файла nxs (формат xml)
+    берёт из файла поле "Server host"
+    выдаёт кортеж - файл в котором ищется и ИП-адрес подключения
+    """
+
+    tree = ET.parse(file)
+    root = tree.getroot()
+    rez = None
+
+    # чтение всех атрибутов в дереве по-очереди
+    # поиск значений адреса "General-Server host" первым
+    for branch in root:
+        if branch.attrib['name'] == 'General':
+            for sub_branch in branch:
+                key = sub_branch.attrib.get('key')
+                val = sub_branch.attrib.get('value')
+                if key == 'Server host':
+                    rez = [file, val]
+    return rez
+
+
+# переход в папку для файлов и поиск в ней файлов
+os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_nxs))
+
+# получение списка файлов nxs в текущей папке
+list_of_nxs_files = get_files_nxs()
+
+# обработка списка файлов
+# если в папке есть файлы, то искать в них адрес и считать их количество чтобы найти дублированные
+if list_of_nxs_files:
+    for full_name_file in list_of_nxs_files:
+        ip_addr = get_ip_from_nxs(full_name_file)[1].strip()
+        print(ip_addr, full_name_file, sep=' ... ')
+else:
+    print()
+    print('файлы nxs в папке не найдены')
+
+
+# print()
+# os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_nxs))
+# for key_ip, val_names in dict_data_nxs_files.items():
+#     new_name = ' '.join(dict_data_nxs_files_good_names[key_ip]).strip()+'_.nxs'
+#     print(val_names)
+#     if len(val_names) > 1:
+#         for real_file in val_names:
+#             if val_names.index(real_file) == 0:
+#                 print('переименовываю "'+real_file+'" в "'+new_name+'"')
+#                 os.rename(real_file, new_name)
+#             else:
+#                 print('удаляю', real_file)
+#                 os.remove(real_file)
+#         print()
